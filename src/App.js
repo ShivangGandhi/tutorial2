36ad5d7383d7a3fb486bbf54c3ae2f3881f3805e
Shivangkumar Gandhi <sh966188@dal.ca>
2023-02-02 15:59:38 -0400

    Tutorial 2 React App

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>
+          This is Shivang's React App for Tutorial 2
+        </h1>
+        <p>----REACT----</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
